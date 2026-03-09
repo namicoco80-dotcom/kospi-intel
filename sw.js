d29efcde3b920a017266f810d08184f6520facda
namicoco80-dotcom <namicoco80@gmail.com>
2026-03-09 12:30:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kospi-intel-v12';
+const CACHE = 'kospi-intel-v13';
 const ASSETS = ['./', './index.html', './prices.json', './news.json', './supply.json', './analysis.json'];
 
 self.addEventListener('install', e => {
